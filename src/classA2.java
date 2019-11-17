ac20fbad58c9da3589be9d68728f1de2e976d8fe
Vivek <vivekpachange1@gmail.com>
2019-11-17 09:53:00 +0530

    static class

---
+++ src/classA2.java
@@ -0,0 +1,17 @@
+
+public class classA2 {
+	static {System.out.println("static block is invoked");}
+	
+	public static void main(String args []) {
+		System.out.println("hello main");
+	} int a1;
+	int b1;
+	int a=8;
+	int b=9;
+	int c=a1+b1;
+	System.out.printIn("sum is +"c);)
+	
+	
+	
+
+}
---
+++ src/lOGINTEXT.java
@@ -0,0 +1,11 @@
+
+public class lOGINTEXT {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","c:\\users\\chromedriver.exe");
+		WebDriver drivrer=new ChromeDriver();
+		// TODO Auto-generated method stub
+
+	}
+
+}
